e4c771999b977818737067dffdf39ea4971cd2cd
unknown <manojkumarcs70@gmail.com>
2023-08-28 18:45:08 +0530

    remove manager.py file

--- djangogirls/models.py
+++ djangogirls/models.py
@@ -2,9 +2,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _ 
-from django.utils.html import mark_safe
-from django.core.validators import RegexValidator
+# from django.utils.translation import gettext_lazy as _ 
 from django.urls import reverse
 from django.utils.text import slugify
 
