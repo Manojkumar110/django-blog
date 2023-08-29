98b13f09080938d55dfc24ce749a928d9b62de26
unknown <manojkumarcs70@gmail.com>
2023-08-30 00:23:16 +0530

    new venv

--- djangogirls/models.py
+++ djangogirls/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# from django.utils.translation import gettext_lazy as _ 
+from django.utils.translation import gettext_lazy as _ 
 from django.urls import reverse
 from django.utils.text import slugify
 
