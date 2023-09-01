c66ed0cc916b0ba85de55db5cc02f08f4160f30e
unknown <manojkumarcs70@gmail.com>
2023-09-01 13:43:53 +0530

    change views in urls

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,17 +1,34 @@
+# from django.urls import path, include
+# from restapi import views
+
+
+# urlpatterns = [
+#     path('post/', views.PostListAPIView.as_view(), name='post'),
+#     path('post/create/', views.PostCreateAPIView.as_view(), name='postcreate'),
+#     path('post/<int:pk>/', views.PostAPIView.as_view(), name='post'),
+#     path('category/', views.CategoryListApiView.as_view(), name='category'),
+#     path('category/<int:pk>/', views.CategoryApiView.as_view(), name='category'),
+#     path('tags/', views.TagsListApiView.as_view(), name='tags'),
+#     path('tags/<int:pk>/', views.TagsApiView.as_view(), name='tags'),
+#     path('user/create/', views.UserListApiView.as_view(), name='usercraete'),
+#     path('user/create/<int:pk>/', views.UserApiView.as_view(), name='userdetail'),
+#     path('comment/', views.CommentListCreateApiView.as_view(), name='comment'),
+#     path('comment/<int:pk>/', views.CommentApiView.as_view(), name='commentdetail'),
+# ]
+
+
 from django.urls import path, include
 from restapi import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('post', views.PostAPIView, basename='post'),
+router.register('category', views.CategoryApiView, basename='category'),
+router.register('tags', views.TagsApiView, basename='tags'),
+router.register('user', views.UserApiView, basename='user'),
+router.register('comment', views.CommentApiView, basename='comment'),
 
 
 urlpatterns = [
-    path('post/', views.PostListAPIView.as_view(), name='post'),
-    path('post/create/', views.PostCreateAPIView.as_view(), name='postcreate'),
-    path('post/<int:pk>/', views.PostAPIView.as_view(), name='post'),
-    path('category/', views.CategoryListApiView.as_view(), name='category'),
-    path('category/<int:pk>/', views.CategoryApiView.as_view(), name='category'),
-    path('tags/', views.TagsListApiView.as_view(), name='tags'),
-    path('tags/<int:pk>/', views.TagsApiView.as_view(), name='tags'),
-    path('user/create/', views.UserListApiView.as_view(), name='usercraete'),
-    path('user/create/<int:pk>/', views.UserApiView.as_view(), name='userdetail'),
-    path('comment/', views.CommentListCreateApiView.as_view(), name='comment'),
-    path('comment/<int:pk>/', views.CommentApiView.as_view(), name='commentdetail'),
+    path('', include(router.urls)),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,63 +1,101 @@
-from djangogirls .models import Post, Category, Tags, User, Comment
+# from djangogirls .models import Post, Category, Tags, User, Comment
+# from restapi .serializers import PostSerializer, CategorySerializre, TagSerializer, UserSerializer, CommentSerializer
+# from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
+# from rest_framework.permissions import IsAuthenticated
+# # Create your views here.
+
+
+# class PostListAPIView(ListAPIView):
+#     # permission_classes = [IsAuthenticated]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class PostCreateAPIView(CreateAPIView):
+#     # permission_classes = [IsAuthenticated]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class PostAPIView(RetrieveUpdateDestroyAPIView):
+#     # permission_classes = [IsAuthenticated]
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class CategoryListApiView(ListCreateAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializre
+
+
+# class CategoryApiView(RetrieveUpdateDestroyAPIView):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializre
+
+
+# class TagsListApiView(ListCreateAPIView):
+#     queryset = Tags.objects.all()
+#     serializer_class = TagSerializer
+
+
+# class TagsApiView(RetrieveUpdateDestroyAPIView):
+#     queryset = Tags.objects.all()
+#     serializer_class = TagSerializer
+
+
+# class UserListApiView(ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+# class UserApiView(RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+# class CommentListCreateApiView(ListCreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+
+# class CommentApiView(RetrieveUpdateDestroyAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
+
+
+from djangogirls .models import Post, Category, Tags, Comment, User
 from restapi .serializers import PostSerializer, CategorySerializre, TagSerializer, UserSerializer, CommentSerializer
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 # Create your views here.
 
 
-class PostListAPIView(ListAPIView):
-    # permission_classes = [IsAuthenticated]
+class PostAPIView(viewsets.ModelViewSet):
+    # permission_classes = ((AllowAny,))
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    http_method_names = ["get", "put", "delete", "patch", "post"]
 
 
-class PostCreateAPIView(CreateAPIView):
-    # permission_classes = [IsAuthenticated]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostAPIView(RetrieveUpdateDestroyAPIView):
-    # permission_classes = [IsAuthenticated]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class CategoryListApiView(ListCreateAPIView):
+class CategoryApiView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializre
+    http_method_names = ["get", "put", "delete", "patch", "post"]
 
 
-class CategoryApiView(RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializre
-
-
-class TagsListApiView(ListCreateAPIView):
+class TagsApiView(viewsets.ModelViewSet):
     queryset = Tags.objects.all()
     serializer_class = TagSerializer
+    http_method_names = ["get", "put", "delete", "patch", "post"]
 
 
-class TagsApiView(RetrieveUpdateDestroyAPIView):
-    queryset = Tags.objects.all()
-    serializer_class = TagSerializer
-
-
-class UserListApiView(ListCreateAPIView):
+class UserApiView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ["get", "put", "delete", "patch", "post"]
 
 
-class UserApiView(RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class CommentListCreateApiView(ListCreateAPIView):
+class CommentApiView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-
-
-class CommentApiView(RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+    http_method_names = ["get", "put", "delete", "patch", "post"]
